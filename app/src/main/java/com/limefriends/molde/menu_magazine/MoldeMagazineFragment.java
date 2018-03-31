66aac252a447c9665587f0d26a64f4fc89c60d2f
leekijung <dlrlwjd1127@naver.com>
2018-04-01 03:01:37 +0900

    기존오류로 재 커밋

--- app/src/main/java/com/limefriends/molde/menu_magazine/MoldeMagazineFragment.java
+++ app/src/main/java/com/limefriends/molde/menu_magazine/MoldeMagazineFragment.java
@@ -17,6 +17,7 @@ public class MoldeMagazineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_molde_magazine, container, false);
     }
 
