7f1a2e0354e5bf16443f73640c61885770bca49e
leekijung <dlrlwjd1127@naver.com>
2018-04-05 16:46:47 +0900

    1차 회의 테스트 수정
    뒤로가기키 모든 프래그먼트에 인터페이스구현

--- app/src/main/java/com/limefriends/molde/menu_mypage/MoldeMyPageFragment.java
+++ app/src/main/java/com/limefriends/molde/menu_mypage/MoldeMyPageFragment.java
@@ -1,14 +1,16 @@
 package com.limefriends.molde.menu_mypage;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.limefriends.molde.MoldeMainActivity;
 import com.limefriends.molde.R;
 
-public class MoldeMyPageFragment extends Fragment {
+public class MoldeMyPageFragment extends Fragment implements MoldeMainActivity.onKeyBackPressedListener{
 
     public static MoldeMyPageFragment newInstance() {
         return new MoldeMyPageFragment();
@@ -20,5 +22,14 @@ public class MoldeMyPageFragment extends Fragment {
         return inflater.inflate(R.layout.mypage_fragment, container, false);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        ((MoldeMainActivity)context).setOnKeyBackPressedListener(this);
+    }
+
+    @Override
+    public void onBackKey() {
+    }
 
 }
