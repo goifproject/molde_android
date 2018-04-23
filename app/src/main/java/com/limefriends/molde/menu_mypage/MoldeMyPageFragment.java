71d52a41e876c6ae1e3627b4bfa61a5d2f8cc31e
leekijung <dlrlwjd1127@naver.com>
2018-04-23 20:43:45 +0900

    Revert "마이페이지 프래그먼트 10차 첫 디자인"
    This reverts commit a6b2a9e898b68c1caa642bce1e4cd43505d9d03c.

--- app/src/main/java/com/limefriends/molde/menu_mypage/MoldeMyPageFragment.java
+++ app/src/main/java/com/limefriends/molde/menu_mypage/MoldeMyPageFragment.java
@@ -1,40 +1,16 @@
 package com.limefriends.molde.menu_mypage;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-import com.alexzh.circleimageview.CircleImageView;
 import com.limefriends.molde.MoldeMainActivity;
 import com.limefriends.molde.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class MoldeMyPageFragment extends Fragment implements MoldeMainActivity.onKeyBackPressedListener{
-    @BindView(R.id.mypage_profile_image)
-    CircleImageView mypage_profile_image;
-    @BindView(R.id.mypage_profile_name)
-    TextView mypage_profile_name;
-    @BindView(R.id.mypage_setting_button)
-    ImageButton mypage_setting_button;
-    @BindView(R.id.mypage_report_button)
-    ImageButton mypage_report_button;
-    @BindView(R.id.mypage_comment_button)
-    ImageButton mypage_comment_button;
-    @BindView(R.id.mypage_scrap_button)
-    ImageButton mypage_scrap_button;
-    @BindView(R.id.mypage_faq_button)
-    Button mypage_faq_button;
-    @BindView(R.id.mypage_log_in_out_button)
-    Button mypage_log_in_out_button;
 
     public static MoldeMyPageFragment newInstance() {
         return new MoldeMyPageFragment();
@@ -43,20 +19,7 @@ public class MoldeMyPageFragment extends Fragment implements MoldeMainActivity.o
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.mypage_fragment, container, false);
-        ButterKnife.bind(this, view);
-
-        // 설정페이지로 이동
-        mypage_setting_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), MoldeMyPageSettings.class);
-                intent.putExtra("title", "설정");
-                startActivity(intent);
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.mypage_fragment, container, false);
     }
 
     @Override
