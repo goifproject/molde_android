c9098474e1b060be2745702d9d26ba6298cf5524
haamseongho <haams704@gmail.com>
2018-05-01 20:28:48 +0900

    마이페이지 FAQ 서버 연동 완료 >> 이메일 null 뜨는거 고쳐야하고 Regex 적용해둠 이거 근데 스레드에서 해야 한 페이지에서 리스너 작용 후에 적용될 것 같음 (null --> 값)

---
+++ app/src/main/java/com/limefriends/molde/Pattern/RegexUtil.java
@@ -0,0 +1,25 @@
+package com.limefriends.molde.Pattern;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by haams on 2018-01-22.
+ */
+
+public class RegexUtil {
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+
+    public static boolean validateEmail(String emalStr){
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emalStr);
+        return matcher.find();
+    }
+    public static final Pattern VALID_PASSWOLD_REGEX_ALPHA_NUM = Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{10,20}$"); // 10~20
+
+    public static boolean validatePassword(String pwStr){
+        Matcher matcher = VALID_PASSWOLD_REGEX_ALPHA_NUM.matcher(pwStr);
+        return matcher.matches();
+    }
+}
