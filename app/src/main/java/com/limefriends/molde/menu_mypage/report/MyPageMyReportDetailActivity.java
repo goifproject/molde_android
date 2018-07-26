247f0f6e1e3a864b6d7a3466472c9e18ae66260c
leekijung <kj.lee@ilts.co.kr>
2018-07-26 15:16:25 +0900

    #24 신고 상세 내역 액티비티 생성

---
+++ app/src/main/java/com/limefriends/molde/menu_mypage/report/MyPageMyReportDetailActivity.java
@@ -0,0 +1,16 @@
+package com.limefriends.molde.menu_mypage.report;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.limefriends.molde.R;
+
+public class MyPageMyReportDetailActivity extends AppCompatActivity {
+
+    //TODO 앞으로 액티비티에 대해 추가 기능이 구현이 필요
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page_my_report_detail);
+    }
+}
