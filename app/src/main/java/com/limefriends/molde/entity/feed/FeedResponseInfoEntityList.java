490acf498d5b3b11327f47c17cd863f4f3d77afe
nadanjo <qskeksq@gmail.com>
2018-08-27 15:09:36 +0900

    지도 코드 정리

---
+++ app/src/main/java/com/limefriends/molde/entity/feed/FeedResponseInfoEntityList.java
@@ -0,0 +1,16 @@
+package com.limefriends.molde.entity.feed;
+
+import java.util.ArrayList;
+
+public class FeedResponseInfoEntityList {
+
+    private ArrayList<FeedResponseInfoEntity> data;
+
+    public ArrayList<FeedResponseInfoEntity> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<FeedResponseInfoEntity> data) {
+        this.data = data;
+    }
+}
